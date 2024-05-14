6d20ff1b9209706b096ad03f90ec70925fb81f26
Rafał Szymanek <rafal.szymanek.89@gmail.com>
2024-05-14 13:05:00 +0200

    corrections from the mentor

--- task2.js
+++ task2.js
@@ -1,11 +1,11 @@
 function formatMessage(message, maxLength) {
     const valueLength = message.length;
     if (valueLength <= maxLength) {
-        console.log(message.slice(0, maxLength))
+        return(message.slice(0, maxLength))
     
     } else {
 
-        console.log(message.slice(0, maxLength) + "...")
+        return(message.slice(0, maxLength) + "...")
        
     }
     
@@ -14,9 +14,9 @@ function formatMessage(message, maxLength) {
 
 
 
-formatMessage("Curabitur ligula sapien", 16); // "Curabitur ligula..."
-formatMessage("Curabitur ligula sapien", 23); // "Curabitur ligula sapien"
-formatMessage("Vestibulum facilisis purus nec", 20); // "Vestibulum facilisis..."
-formatMessage("Vestibulum facilisis purus nec", 30); // "Vestibulum facilisis purus nec"
-formatMessage("Nunc sed turpis a felis in nunc fringilla", 15); // "Nunc sed turpis..."
-formatMessage("Nunc sed turpis a felis in nunc fringilla", 41); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
--- task4.js
+++ task4.js
@@ -4,26 +4,26 @@ function getShippingCost(country) {
     switch (country) {
         case "Australia": {
             price = 170;
-            console.log(`Shipping to ${country} will cost ${price} credits`);
-            break;
+            return(`Shipping to ${country} will cost ${price} credits`);
+            
         }
         case "China": {
             price = 100;
-            console.log(`Shipping to ${country} will cost ${price} credits`);
-            break;
+            return(`Shipping to ${country} will cost ${price} credits`);
+            
         }
         case "Chile": {
             price = 250;
-            console.log(`Shipping to ${country} will cost ${price} credits`);
-            break;
+            return(`Shipping to ${country} will cost ${price} credits`);
+            
         } 
         case "Jamaica": {
             price = 120;
-            console.log(`Shipping to ${country} will cost ${price} credits`);
-            break;
+            return(`Shipping to ${country} will cost ${price} credits`);
+            
         }
         default: {
-            console.log("Sorry, there is no delivery to your country");
+            return("Sorry, there is no delivery to your country");
         }
     }
 }
@@ -31,9 +31,9 @@ function getShippingCost(country) {
 
 
 
-getShippingCost("Australia"); // "Shipping to Australia will cost 170 credits"
-getShippingCost("Germany"); // "Sorry, there is no delivery to your country"
-getShippingCost("China"); // "Shipping to China will cost 100 credits"
-getShippingCost("Chile"); // "Shipping to Chile will cost 250 credits"
-getShippingCost("Jamaica"); // "Shipping to Jamaica will cost 120 credits"
-getShippingCost("Sweden"); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
